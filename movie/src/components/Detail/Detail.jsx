94bf70cabf4ebb29fecfe1515faa94c17be9ac3c
Mathias <mat.derache@gmail.com>
2021-02-25 14:30:57 +0100

    fin detail

--- movie/src/components/Detail/Detail.jsx
+++ movie/src/components/Detail/Detail.jsx
@@ -19,16 +19,29 @@ export class Detail extends Component {
     render() {
         console.log(this.state.array);
         return (
-            <React.Fragment>
+            <div className="container pageDetail">
                 <div className="poster">
-                   <img src={"https://image.tmdb.org/t/p/w500" + this.state.array.poster_path} alt="poster"/>
+                   <img src={"https://image.tmdb.org/t/p/w500" + this.state.array.poster_path} alt="poster" className="posterDetail"/>
                 </div>
-                <div className="informations">
-                    {this.state.array.title}
-                    {this.state.array.overview}
-                    {this.state.array.release_date}
+                <div className="informationsDetail">
+                    <div className="infoDetail">
+                        <h1 className="text-center">Title :</h1>
+                        {this.state.array.title}
+                    </div>
+                    <div className="infoDetail">
+                        <h1 className="text-center">Overview :</h1>
+                        {this.state.array.overview}
+                    </div>
+                    <div className="infoDetail">
+                        <h1 className="text-center">Release Date :</h1>
+                        {this.state.array.release_date}
+                    </div>
+                    <div className="infoDetail">
+                        <h1 className="text-center">Note :</h1>
+                        {this.state.array.vote_average} / 10
+                    </div>
                 </div>
-            </React.Fragment>
+            </div>
         )
     }
 }
